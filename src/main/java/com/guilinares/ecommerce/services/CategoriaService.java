231fa30a6f790fc8530059f4ce534d0ca9d15fb7
Breuzin <guimine.20@gmail.com>
2022-03-22 13:54:22 -0300

    Criados repository e service para Categoria

---
+++ src/main/java/com/guilinares/ecommerce/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.guilinares.ecommerce.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guilinares.ecommerce.domain.Categoria;
+import com.guilinares.ecommerce.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElse(null);	
+	}
+}
