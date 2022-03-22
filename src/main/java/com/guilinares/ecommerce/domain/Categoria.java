77fe1c300347ad117b82eea0f37d451b87ce4ae6
Breuzin <guimine.20@gmail.com>
2022-03-22 12:13:52 -0300

    Banco de Dados H2 e criação automática da base de dados

--- src/main/java/com/guilinares/ecommerce/domain/Categoria.java
+++ src/main/java/com/guilinares/ecommerce/domain/Categoria.java
@@ -3,9 +3,17 @@ package com.guilinares.ecommerce.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = -3557971464338410089L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
