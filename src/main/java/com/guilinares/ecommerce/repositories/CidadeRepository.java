50245ba65e63392dac3456809e44d543a0062930
Breuzin <guimine.20@gmail.com>
2022-03-22 19:34:51 -0300

    Estado e Cidade implementadas

---
+++ src/main/java/com/guilinares/ecommerce/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.guilinares.ecommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.guilinares.ecommerce.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+	
+}
