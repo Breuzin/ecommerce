29e244ce2c6921b0c7e5cfe7d64d64a0ea5d7c55
Breuzin <guimine.20@gmail.com>
2022-03-25 08:39:31 -0300

    Pedido, Pagamento, EstadoPagamento implementados

---
+++ src/main/java/com/guilinares/ecommerce/domain/PagamentoComCartao.java
@@ -0,0 +1,28 @@
+package com.guilinares.ecommerce.domain;
+
+import javax.persistence.Entity;
+
+import com.guilinares.ecommerce.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComCartao extends Pagamento{
+	private static final long serialVersionUID = -4744149789674528922L;
+
+	private Integer numeroDeParcelas;
+	
+	public PagamentoComCartao() {
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+		super(id, estado, pedido);
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+}
