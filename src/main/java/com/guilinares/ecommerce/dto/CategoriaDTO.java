4b7fb3a68e2684605827b39a159169564e944a78
Breuzin <guimine.20@gmail.com>
2022-03-26 12:07:45 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/guilinares/ecommerce/dto/CategoriaDTO.java
+++ src/main/java/com/guilinares/ecommerce/dto/CategoriaDTO.java
@@ -2,12 +2,20 @@ package com.guilinares.ecommerce.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.guilinares.ecommerce.domain.Categoria;
+
 
 public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = -3536265313553932780L;
 
 	private Integer id;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
--- src/main/java/com/guilinares/ecommerce/services/CategoriaService.java
+++ src/main/java/com/guilinares/ecommerce/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.guilinares.ecommerce.domain.Categoria;
+import com.guilinares.ecommerce.dto.CategoriaDTO;
 import com.guilinares.ecommerce.repositories.CategoriaRepository;
 import com.guilinares.ecommerce.services.exceptions.DataIntegrityException;
 import com.guilinares.ecommerce.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
