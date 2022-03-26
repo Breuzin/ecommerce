d804e25272038b1669059c3c088ac7a027b12582
Breuzin <guimine.20@gmail.com>
2022-03-26 12:33:51 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/com/guilinares/ecommerce/resources/exceptions/ValidationError.java
@@ -0,0 +1,25 @@
+package com.guilinares.ecommerce.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError{
+	private static final long serialVersionUID = 3154581498167933327L;
+
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addErrors(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+	
+	
+
+}
