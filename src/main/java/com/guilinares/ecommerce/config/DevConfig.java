fe6bba089b47782ff35a20470aaea9c9bcafe64d
Breuzin <guimine.20@gmail.com>
2022-04-07 11:59:43 -0300

    Email

--- src/main/java/com/guilinares/ecommerce/config/DevConfig.java
+++ src/main/java/com/guilinares/ecommerce/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.guilinares.ecommerce.services.DBService;
+import com.guilinares.ecommerce.services.EmailService;
+import com.guilinares.ecommerce.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 		
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
